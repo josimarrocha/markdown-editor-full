1499160f7503abba9f5fac8a1e8bbcbf81225381
Josimar <rocha_josimar@yahoo.com.br>
2019-07-24 20:25:37 -0300

    Create component to list files saved and add style

---
+++ src/components/ListFiles/index.js
@@ -0,0 +1,40 @@
+import React from 'react'
+import './styles.css'
+
+const ListFiles = ({ isListFileOpen, listFilesToggle, files, setFile, setTextMarkdown, deleteFile }) => {
+  const handleEditFile = (id) => {
+    setFile({
+      id: files[id].id,
+      nameFile: files[id].nameFile,
+      text: files[id].text
+    })
+    setTextMarkdown(files[id].text)
+  }
+
+  const handleDeletFile = (id) => {
+    deleteFile(id)
+  }
+  return (
+    <div className={`container-list-files ${isListFileOpen && 'active'}`}>
+      <div className="close-list" onClick={() => listFilesToggle()}>
+        <i className="far fa-times-circle"></i>
+      </div>
+      <div className="list-files-content">
+        <h3>Arquivos salvos</h3>
+        <ul className='list-files'>
+          {Object.keys(files).map(file => (
+            <li key={files[file].id}>
+              <span>{files[file].nameFile}</span>
+              <div className="actions">
+                <i className="fas fa-edit" onClick={() => handleEditFile(files[file].id)}></i>
+                <i className="fas fa-trash" onClick={() => handleDeletFile(files[file].id)}></i>
+              </div>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  )
+}
+
+export default ListFiles
