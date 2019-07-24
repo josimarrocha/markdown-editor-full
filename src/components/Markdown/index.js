6357b9c46570040dd4b645db6acfdd0303e43491
Josimar <rocha_josimar@yahoo.com.br>
2019-07-24 20:27:04 -0300

    Update config useEffect and update styles

--- src/components/Markdown/index.js
+++ src/components/Markdown/index.js
@@ -1,12 +1,30 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import cm from 'codemirror'
 import './styles.css'
 import 'codemirror/lib/codemirror.css'
 import 'codemirror/mode/markdown/markdown'
 
-const MarkdownEditor = ({ refTextarea }) => {
+const MarkdownEditor = ({ onChangeCodeMirror, file }) => {
+  const textareaRef = React.createRef()
+
+  useEffect(() => {
+    cm.fromTextArea(textareaRef.current, {
+      lineNumbers: true,
+      mode: 'markdown',
+      autofocus: true,
+      lineWrapping: true,
+    })
+      .on('focus', (e) => {
+        onChangeCodeMirror(e)
+      })
+  }, [file.id])
   return (
     <div className='markdown-editor'>
-      <textarea ref={refTextarea} />
+      <textarea
+        ref={textareaRef}
+        value={file.text}
+        onChange={value => value}
+      />
     </div>
   )
 }
