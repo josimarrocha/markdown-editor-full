48b93d107ccf0b99504d51da25b7d96e10db34e0
Josimar <rocha_josimar@yahoo.com.br>
2019-07-24 20:28:51 -0300

    Update menu

--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -1,21 +1,25 @@
 import React from 'react'
 import { connect } from 'react-redux'
-import { menuToggle } from '../../reducers/ui/actionsCreators'
+import { menuToggle, modalToggle, listFilesToggle } from '../../reducers/ui/actionsCreators'
 import './styles.css'
 
-const Menu = ({ isMenuOpen, menuToggle }) => {
+const Menu = ({ isMenuOpen, menuToggle, modalToggle, listFilesToggle, loadingFilesStorage }) => {
   return (
     <nav className={`${isMenuOpen && 'menuActive'}`}>
       <div className="container-menu">
-        <div className="close-menu" onClick={menuToggle}>
+        <div className="close-menu" onClick={() => menuToggle(false)}>
           <i className="far fa-times-circle"></i>
         </div>
         <div className="menu-title">
           <h3>Menu</h3>
         </div>
         <ul className='menu-list'>
-          <li>Novo arquivo</li>
-          <li>Editar arquivo</li>
+          <li onClick={() => modalToggle(true)}>Novo arquivo</li>
+          <li onClick={() => {
+            listFilesToggle(true)
+            menuToggle()
+          }
+          }>Arquivos salvos</li>
         </ul>
       </div>
     </nav>
@@ -26,4 +30,9 @@ const mapStateToProps = state => ({
   isMenuOpen: state.ui.isMenuOpen
 })
 
-export default connect(mapStateToProps, { menuToggle })(Menu)
+export default connect(mapStateToProps,
+  {
+    menuToggle,
+    modalToggle,
+    listFilesToggle
+  })(Menu)
