0279bc2cdb24fded52c2440d43050b0ccff9c2eb
Josimar <rocha_josimar@yahoo.com.br>
2019-07-24 20:38:23 -0300

    Delete page new

--- src/routes.js
+++ src/routes.js
@@ -1,12 +1,10 @@
 import React from 'react'
 import { Route, Switch } from 'react-router-dom'
 import Home from './pages/Home'
-import New from './pages/New'
 import 'normalize.css'
 const Routes = () => (
   <Switch>
     <Route path='/' exact component={Home} />
-    <Route path='/new' exact component={New} />
   </Switch>
 )
 
