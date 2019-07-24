8afd6374ea67eccd46e37d8dd89b288afcaa13f8
Josimar <rocha_josimar@yahoo.com.br>
2019-07-24 20:24:33 -0300

    Update add  views containers buttons and styled

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,23 +1,39 @@
 import React, { useState, useEffect } from 'react'
 import { connect } from 'react-redux'
-import { menuToggle } from '../../reducers/ui/actionsCreators'
+import { menuToggle, viewsContainerToggle } from '../../reducers/ui/actionsCreators'
 import './styles.css'
 
-const Header = ({ menuToggle, saving }) => {
+const Header = ({ menuToggle, ui, viewsContainerToggle }) => {
   const [isChangeMarkdown, setIsChangeMarkdown] = useState(false)
+  const [views, setVisibleViews] = useState({
+    markIn: null,
+    markOut: null
+  })
   useEffect(() => {
-    if (saving.lengthMarkdown > 0) {
+    setVisibleViews({ ...ui.containersView })
+    if (ui.lengthMarkdown > 0) {
       setIsChangeMarkdown(true)
     }
-  }, [saving])
+  }, [ui])
   return (
     <header>
       <div className="container-header">
         <h1>Markdown Editor</h1>
-        <div className="text-saving">
-          <p>{!isChangeMarkdown ? '' : saving.isSaving ? 'Salvando...' : 'Salvo!'}</p>
+        {isChangeMarkdown && <div className="text-saving">
+          <p>{ui.isSaving ? 'Salvando...' : 'Salvo!'}</p>
+        </div>}
+        <div className="toggle-containers">
+          <span
+            className={`${views.markIn && !views.markOut ? 'markIn active' : 'markIn'}`}
+            onClick={() => viewsContainerToggle(true, false)} />
+          <span
+            className={`${views.markIn && views.markOut ? 'both active' : 'both'}`}
+            onClick={() => viewsContainerToggle(true, true)} />
+          <span
+            className={`${!views.markIn && views.markOut ? 'markOut active' : 'markOut'}`}
+            onClick={() => viewsContainerToggle(false, true)} />
         </div>
-        <span className="icone-menu" onClick={menuToggle}>
+        <span className="icone-menu" onClick={() => menuToggle(true)}>
           <i className="fas fa-bars"></i>
         </span>
       </div>
@@ -25,7 +41,7 @@ const Header = ({ menuToggle, saving }) => {
   )
 }
 const mapStateToProps = state => ({
-  saving: state.ui
+  ui: state.ui
 })
 
-export default connect(mapStateToProps, { menuToggle })(Header)
+export default connect(mapStateToProps, { menuToggle, viewsContainerToggle })(Header)
