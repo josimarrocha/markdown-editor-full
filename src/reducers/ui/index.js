0a2a92549b2c0dc89f1f62b199c3c54c36fa91ab
Josimar <rocha_josimar@yahoo.com.br>
2019-07-24 20:41:38 -0300

    Update reducer ui

--- src/reducers/ui/index.js
+++ src/reducers/ui/index.js
@@ -1,9 +1,21 @@
-import { MENU_TOGGLE, SAVING_CHANGE } from './actionsCreators'
+import {
+  MENU_TOGGLE,
+  SAVING_CHANGE,
+  MODAL_TOGGLE,
+  LIST_FILES_TOGGLE,
+  CONTAINERS_MARKDOWN_TOGGLE
+} from './actionsCreators'
 
 const initialState = {
   isMenuOpen: false,
   isSaving: false,
-  lengthMarkdown: 0
+  lengthMarkdown: 0,
+  isModalOpen: false,
+  isListFileOpen: false,
+  containersView: {
+    markIn: true,
+    markOut: true
+  }
 }
 
 const ui = (state = initialState, action) => {
@@ -11,13 +23,32 @@ const ui = (state = initialState, action) => {
     case MENU_TOGGLE:
       return {
         ...state,
-        isMenuOpen: !state.isMenuOpen
+        isMenuOpen: action.payload
+      }
+    case LIST_FILES_TOGGLE:
+      return {
+        ...state,
+        isListFileOpen: action.payload
       }
     case SAVING_CHANGE:
       return {
         ...state,
         isSaving: action.payload.isSaving,
         lengthMarkdown: action.payload.length === 'vazio' ? state.lengthMarkdown : action.payload.length
+      }
+    case MODAL_TOGGLE:
+      return {
+        ...state,
+        isModalOpen: action.payload
+      }
+    case CONTAINERS_MARKDOWN_TOGGLE:
+      return {
+        ...state,
+        containersView: {
+          ...state.containersView,
+          markIn: action.payload.markIn,
+          markOut: action.payload.markOut
+        }
       }
     default:
       return state
