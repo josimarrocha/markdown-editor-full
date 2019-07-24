9416ba79c7574f52e9886e1504c0817d8a33fbd9
Josimar <rocha_josimar@yahoo.com.br>
2019-07-24 20:41:19 -0300

    Update actionsCreators ui

--- src/reducers/ui/actionsCreators.js
+++ src/reducers/ui/actionsCreators.js
@@ -1,9 +1,37 @@
 export const MENU_TOGGLE = 'markdown:MENU_TOGGLE'
 export const SAVING_CHANGE = 'markdown:SAVING_CHANGE'
+export const MODAL_TOGGLE = 'markdown:MODAL_TOGGLE'
+export const LIST_FILES_TOGGLE = 'markdown:LIST_FILES_TOGGLE'
+export const CONTAINERS_MARKDOWN_TOGGLE = 'markdown:CONTAINERS_MARKDOWN_TOGGLE'
 
-export const menuToggle = () => dispatch => {
+export const menuToggle = (boolean = false) => dispatch => {
   dispatch({
-    type: MENU_TOGGLE
+    type: MENU_TOGGLE,
+    payload: boolean
+  })
+}
+
+export const modalToggle = (open = false) => dispatch => {
+  dispatch({
+    type: MODAL_TOGGLE,
+    payload: open
+  })
+}
+
+export const listFilesToggle = (open = false) => dispatch => {
+  dispatch({
+    type: LIST_FILES_TOGGLE,
+    payload: open
+  })
+}
+
+export const viewsContainerToggle = (markIn, markOut) => dispatch => {
+  dispatch({
+    type: CONTAINERS_MARKDOWN_TOGGLE,
+    payload: {
+      markIn,
+      markOut
+    }
   })
 }
 
