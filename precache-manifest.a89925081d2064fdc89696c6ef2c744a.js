ecefb995b3816cf1b27d428ffc9ce3a474a18628
josimarrocha <rocha_josimar@yahoo.com.br>
2019-08-10 02:06:09 +0000

    Updates

---
+++ precache-manifest.a89925081d2064fdc89696c6ef2c744a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b1aad6b0686b3fd548a7605088369f06",
+    "url": "/markdown-editor-full/index.html"
+  },
+  {
+    "revision": "5fca64ccb7bfb0d3b4dc",
+    "url": "/markdown-editor-full/static/css/3.130bb9e9.chunk.css"
+  },
+  {
+    "revision": "cc6a01053020276be79f",
+    "url": "/markdown-editor-full/static/css/main.744e39d3.chunk.css"
+  },
+  {
+    "revision": "56a7710e499c89286486",
+    "url": "/markdown-editor-full/static/js/2.1496184f.chunk.js"
+  },
+  {
+    "revision": "5fca64ccb7bfb0d3b4dc",
+    "url": "/markdown-editor-full/static/js/3.bf5ec07c.chunk.js"
+  },
+  {
+    "revision": "cc6a01053020276be79f",
+    "url": "/markdown-editor-full/static/js/main.70c72bd1.chunk.js"
+  },
+  {
+    "revision": "bf6e1a6a32d5b4c1cae0",
+    "url": "/markdown-editor-full/static/js/runtime~main.c0bdf500.js"
+  }
+]);
